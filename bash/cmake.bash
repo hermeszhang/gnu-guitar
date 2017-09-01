d7ddc9fb847aba01e0e55d7d2a83f2ee69600358
Taylor Holberton <taylorcholberton@gmail.com>
2017-08-31 18:15:24 -0700

    added CMake options and cmake_clean()

--- bash/cmake.bash
+++ bash/cmake.bash
@@ -5,6 +5,14 @@ function cmake_build {
 	install_dir="$root/package"
 	mkdir -p "$binary_dir"
 	cd "$binary_dir"
-	cmake "$source_dir" -DCMAKE_INSTALL_PREFIX="$install_dir"
+	cmake "$source_dir" -DCMAKE_INSTALL_PREFIX="$install_dir" \
+	                    -DCMAKE_PREFIX_PATH="$install_dir" \
+	                    -DBUILD_SHARED_LIBS=OFF
 	cmake --build . --target install
+}
+
+function cmake_clean {
+	project=$1
+	binary_dir="$root/build/$project"
+	cmake --build "$binary_dir" --target clean
 }
