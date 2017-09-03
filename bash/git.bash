d2bed30a8a288a22c70aa0251e0e0865b25bda64
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-02 21:14:06 -0400

    fixed git pull

--- bash/git.bash
+++ bash/git.bash
@@ -1,10 +1,13 @@
 # Clones a repo if it doesn't exist,
 # updates it otherwise.
 function git_repo_update {
+	original_dir="$PWD"
 	repo_src=$1
 	repo_dst=$2
 	if [ -e "$repo_dst" ]; then
-		git --git-dir="$repo_dst/.git" pull origin master
+		cd "$repo_dst"
+		git pull origin master
+		cd "$original_dir"
 	else
 		git clone "$repo_src" "$repo_dst"
 	fi
