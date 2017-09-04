26ad11443ff3a12609ed24e756c4b26157f92ca7
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-04 14:52:14 -0400

    added artwork repository and rename gnu-guitar-core

--- setup.bash
+++ setup.bash
@@ -13,7 +13,8 @@ mkdir -p "repositories"
 mkdir -p "build"
 mkdir -p "package"
 
-git_repo_update "https://github.com/gnu-guitar/gnu-guitar-core.git" "repositories/gnu-guitar-core"
+git_repo_update "https://github.com/gnu-guitar/artwork.git" "repositories/artwork"
+git_repo_update "https://github.com/gnu-guitar/gnu-guitar-core.git" "repositories/core"
 git_repo_update "https://github.com/gnu-guitar/gnu-guitar-qt.git" "repositories/gnu-guitar-qt"
 
 ./build.bash
