fa3c9f78859e2f91a62afdfa2147ef63df5295db
Taylor Holberton <taylorcholberton@gmail.com>
2017-09-16 17:54:56 -0400

    setting LD_LIBRARY_PATH

--- run.bash
+++ run.bash
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-LADSPA_PATH="$PWD/package/lib/ladspa"
+LADSPA_PATH+="$PWD/package/lib/ladspa"
+
+LD_LIBRARY_PATH+="$PWD/package/lib"
 
 export LADSPA_PATH
+export LD_LIBRARY_PATH
 
 package/bin/gnu-guitar-qt
